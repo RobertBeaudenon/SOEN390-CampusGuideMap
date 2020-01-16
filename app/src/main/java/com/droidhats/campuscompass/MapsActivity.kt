0d07166214ccf36550cbc8ccb92ef50b59e19530
Nicholas Nagy <ygannicholas@gmail.com>
2020-01-16 10:17:31 -0500

    Moved map to center on concordia GPS coordinate and added title to point on map

--- app/src/main/java/com/droidhats/campuscompass/MapsActivity.kt
+++ app/src/main/java/com/droidhats/campuscompass/MapsActivity.kt
@@ -1,18 +1,23 @@
 package com.droidhats.campuscompass
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+//OnMapReadyCallback : interface ; extends AppCompatActivity() ;  GoogleMap.OnMarkerClickListener interface, which defines the onMarkerClick(), called when a marker is clicked or tapped:
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,  GoogleMap.OnMarkerClickListener {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     /**
@@ -33,11 +40,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
 
-        // Add a marker in Sydney and move the camera
+/*        // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))*/
+
+        val myPlace = LatLng(45.495637, -73.578235)  // this is concordia latitude and longitude coordinate
+        map.addMarker(MarkerOptions().position(myPlace).title("Concordia"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(myPlace, 15.0f)) //centers marker on the screen, and control zoom on map: 0 full zoom out 20 max zoom in
+
+        //enable the zoom controls on the map and declare MapsActivity as the callback triggered when the user clicks a marker on this map
+        map.getUiSettings().setZoomControlsEnabled(true)
+        map.setOnMarkerClickListener(this)
     }
+
+//implements methods of interface   GoogleMap.OnMarkerClickListener
+override fun onMarkerClick(p0: Marker?) = false
 }
