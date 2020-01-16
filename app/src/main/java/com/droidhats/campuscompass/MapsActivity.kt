76ac74cb37fcdc6ff5a4d3813d4bdbf223d5457f
robertbeaudenon <robertbeaudenon@gmail.com>
2020-01-16 01:36:33 -0500

    Added address view when clicking on icon

--- app/src/main/java/com/droidhats/campuscompass/MapsActivity.kt
+++ app/src/main/java/com/droidhats/campuscompass/MapsActivity.kt
@@ -1,8 +1,11 @@
 package com.droidhats.campuscompass
 
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -14,6 +17,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import java.io.IOException
 
 //OnMapReadyCallback : interface ; extends AppCompatActivity() ;  GoogleMap.OnMarkerClickListener interface, which defines the onMarkerClick(), called when a marker is clicked or tapped:
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback,  GoogleMap.OnMarkerClickListener {
@@ -116,8 +120,39 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,  GoogleMap.OnMarke
     private fun placeMarkerOnMap(location: LatLng) {
         // 1 Create a MarkerOptions object and sets the user’s current location as the position for the marker
         val markerOptions = MarkerOptions().position(location)
+
+       //added a call to getAddress() and added this address as the marker title.
+       val titleStr = getAddress(location)
+       markerOptions.title(titleStr)
+
+
         // 2 Add the marker to the map
         map.addMarker(markerOptions)
+    }
+
+    //This method get address from coordinates
+    private fun getAddress(latLng: LatLng): String {
+        // 1 Creates a Geocoder object to turn a latitude and longitude coordinate into an address and vice versa
+        val geocoder = Geocoder(this)
+        val addresses: List<Address>?
+        val address: Address?
+        var addressText = ""
+
+        try {
+            // 2 Asks the geocoder to get the address from the location passed to the method
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+            // 3 If the response contains any address, then append it to a string and return
+            if (null != addresses && !addresses.isEmpty()) {
+                address = addresses[0]
+                for (i in 0 until address.maxAddressLineIndex) {
+                    addressText += if (i == 0) address.getAddressLine(i) else "\n" + address.getAddressLine(i)
+                }
+            }
+        } catch (e: IOException) {
+            Log.e("MapsActivity", e.localizedMessage)
+        }
+
+        return addressText
     }
 
 }
