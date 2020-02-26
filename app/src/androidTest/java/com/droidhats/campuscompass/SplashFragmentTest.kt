5944b3d8fd45caeac3248e1a9782f432830ebbdf
Amanda Kolopanis <amandakolopa@gmail.com>
2020-02-26 11:13:43 -0500

    Updated UI tests. It was producing a failed test for displaying the splash screen images. Reason was navigation was not in the test to open the fragment.

--- app/src/androidTest/java/com/droidhats/campuscompass/SplashFragmentTest.kt
+++ app/src/androidTest/java/com/droidhats/campuscompass/SplashFragmentTest.kt
@@ -19,14 +19,38 @@ class SplashFragmentTest {
 
     @get:Rule
     var activityRule: ActivityTestRule<MainActivity>
-            = ActivityTestRule(MainActivity::class.java)
+    = ActivityTestRule(MainActivity::class.java)
 
     val navController = TestNavHostController(
         ApplicationProvider.getApplicationContext())
 
+    //Tests if all images appear on view
+    @Test
+    fun test_isFragmentImagesInView() {
+        //needs to be here to see which fragment is being opened
+        navController.setGraph(R.navigation.navigation)
+
+        //tests if splash_fragment is in view
+        assertEquals(navController.currentDestination?.id, R.id.splash_fragment)
+
+        //sees if the images display
+        onView(withId(R.id.splash_screen))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.cc_logo))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.cc_title))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.cc_university))
+            .check(matches(isDisplayed()))
+    }
+
     //Tests possible navigation paths from splash_fragment
     @Test
     fun test_isNavigationCorrect() {
+
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext())
+
         navController.setGraph(R.navigation.navigation)
 
         //tests if splash_fragment is in view
@@ -43,19 +67,5 @@ class SplashFragmentTest {
         //tests if there is navigation to the map_fragment
         navController.navigate(R.id.action_splashFragment_to_mapsActivity)
         assertEquals(navController.currentDestination?.id, R.id.map_fragment)
-    }
-
-    //Tests if all images appear on view
-    @Test
-    fun test_isFragmentImagesInView() {
-        //sees if the images display
-        onView(withId(R.id.splash_screen))
-            .check(matches(isDisplayed()))
-        onView(withId(R.id.cc_logo))
-            .check(matches(isDisplayed()))
-        onView(withId(R.id.cc_title))
-            .check(matches(isDisplayed()))
-        onView(withId(R.id.cc_university))
-            .check(matches(isDisplayed()))
     }
 }
