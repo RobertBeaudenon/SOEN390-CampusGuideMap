4b53975301b5f56d890a77e11ed2e50fcf5555b6
Jeremiah <jeremiahteee@gmail.com>
2020-02-26 17:40:58 -0500

    Issue #2 & #5  JSON for buildings
    - added buildings.json, ideally we would have all the coordinates of polygon edges to each building in this file
    - modified Campus parameter to accept the json (so it could handle it) - might change in the future
    - added getPolygonOptions() method that will be used in drawBuildingPolygons() from MapFragment.kt (instead of the duplicate code)
    
    in this commit I was able to read the buildings.json file & print its content to Logcat/debug
    
    Next up:
    - I will hard-code the coordinates in drawBuildingPolygons() like Makram did
     (highlight all the buildings using duplicate code)
    
    TO-DO:
    have all the coordinates in the json file 
    figure out how to parse the json 
    refactor drawBuildingPolygons()

--- app/src/main/java/com/droidhats/campuscompass/models/Location.kt
+++ app/src/main/java/com/droidhats/campuscompass/models/Location.kt
@@ -1,6 +1,10 @@
 package com.droidhats.campuscompass.models
 
+import android.util.Log
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Polygon
+import com.google.android.gms.maps.model.PolygonOptions
+import org.json.JSONObject
 
 /*
 * Model for location classes
@@ -12,17 +16,42 @@ abstract class Location(coordinate: LatLng) {
 }
 
 //
-class Campus(coordinate: LatLng, name: String) : Location (coordinate) {
-    private lateinit var buildings: Array<Building>
+class Campus(coordinate: LatLng, name: String, jsonObject: JSONObject) : Location (coordinate){
+    private lateinit var buildingsList: List<Building>
     private val name: String = name
+    private val jsonObject: JSONObject = jsonObject
     fun getName(): String = name
+
+    fun createBuildings(){
+        /*TO-DO:
+        - Move coordinates to buildings.json
+        - Parse building info from JSON, create buildings & populate buildingsList
+         */
+        Log.d("JSON is", jsonObject.getString("buildings")) //so far this works, JSON is printed on Logcat/debug
+    }
 }
 
 // Model for building class, data relating to buildings should be stored here
 class Building(coordinate: LatLng, name: String, desc: String) : Location (coordinate) {
     private val name: String = name
     private val desc: String = desc
+    private val polygonColor = 4289544510.toInt()
+    private lateinit var polygon : Polygon
+    private lateinit var polygonCoordinatesList: List<LatLng>
 
     fun getName(): String = name
     fun getDescription(): String = desc
+
+    fun getPolygonOptions(): PolygonOptions{
+
+        var polygonOptions = PolygonOptions()
+            .fillColor(polygonColor)
+            .clickable(true)
+
+        for(polygonCoordinate in polygonCoordinatesList){
+            polygonOptions.add(polygonCoordinate)
+        }
+
+        return polygonOptions
+    }
 }
