ef4dcce1c903edeb82efdb21d60e80a63286af3e
makchamp <makramadaime@hotmail.com>
2020-02-26 11:27:54 -0500

    Issues #13 & #14: Calendar Fragment UI, selecting/filtering calendars with dialog
    -Added a button that allow users to select the calendars they want to display
    -THe button opens  a dialog fragment with a list of radio button representing the colors in the google calendar app
    -I only save the state of the dialog for now

--- app/src/main/java/com/droidhats/campuscompass/viewmodels/CalendarViewModel.kt
+++ app/src/main/java/com/droidhats/campuscompass/viewmodels/CalendarViewModel.kt
@@ -1,11 +1,14 @@
 package com.droidhats.campuscompass.viewmodels
 
 import android.app.Application
+import android.content.SharedPreferences
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import com.droidhats.campuscompass.models.Calendar
 import com.droidhats.campuscompass.repositories.CalendarRepository
+import com.droidhats.campuscompass.views.CalendarFragment
 
 /*
   This class must extend AndroidViewModel instead of just ViewModel because
@@ -15,19 +18,38 @@ class CalendarViewModel(application: Application) : AndroidViewModel(application
 
     private val context = getApplication<Application>().applicationContext
     private var userCalendars = MutableLiveData<MutableMap<String, Calendar>>()
+    private var selectedCalendars = MutableLiveData<ArrayList<Calendar>>()
 
     private val _text = MutableLiveData<String>().apply {
         value = "Calendar"
     }
     val text: LiveData<String> = _text
 
-   fun init()
-    {
-       userCalendars = CalendarRepository.getInstance().getCalendars(context)
+   fun init() {
+      userCalendars = CalendarRepository.getInstance().getCalendars(context)
     }
 
-    fun getUserCalendars() : MutableLiveData<MutableMap<String, Calendar>>
+    fun getCalendars() : MutableLiveData<ArrayList<Calendar>>
     {
-       return userCalendars
+       return selectedCalendars
     }
+
+    fun selectCalendars(selectedColors: BooleanArray) {
+
+        val colorArray = CalendarFragment.googleCalendarColorMap.keys.toTypedArray()
+        val filteredList = arrayListOf<Calendar>()
+        //Get the Calendars of the selected colors
+        for (i in selectedColors.indices) {
+            if (selectedColors[i]) {
+                val colorToAdd =
+                    userCalendars.value!![CalendarFragment.googleCalendarColorMap[colorArray[i]]]
+                if (colorToAdd != null)
+                    filteredList.add(colorToAdd)
+            }
+        }
+        selectedCalendars.value = filteredList
+
+    }
+
 }
+
--- app/src/main/java/com/droidhats/campuscompass/views/CalendarFragment.kt
+++ app/src/main/java/com/droidhats/campuscompass/views/CalendarFragment.kt
@@ -1,62 +1,107 @@
 package com.droidhats.campuscompass.views
 
 import android.Manifest
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
-import androidx.activity.OnBackPressedCallback
 import androidx.core.content.ContextCompat.checkSelfPermission
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.SavedStateViewModelFactory
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.droidhats.campuscompass.viewmodels.CalendarViewModel
 import com.droidhats.campuscompass.R
 import com.droidhats.campuscompass.adapters.CalendarAdapter
-import com.droidhats.campuscompass.models.Calendar
 import com.droidhats.campuscompass.models.CalendarEvent
+import com.droidhats.campuscompass.viewmodels.CalendarViewModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
-class CalendarFragment : Fragment() {
+
+class CalendarFragment : DialogFragment() {
 
     private lateinit var calendarViewModel: CalendarViewModel
-
+    private lateinit var recyclerView: RecyclerView
     private var columnCount = 1
 
     companion object {
         private const val READ_CALENDAR_PERMISSION_REQUEST_CODE = 1
+
         // This callback could also be private and be set on the host(main) Activity
         var onCalendarEventClickListener: OnCalendarEventClickListener? = null
+
+    //The colors available on the google calendar app with their corresponding int value
+         val googleCalendarColorMap : Map<String, String> = mapOf(
+            "Default" to "-6299161",
+            "Tomato" to "-2350809",
+            "Tangerine" to "-18312",
+            "Banana" to "-272549",
+            "Basil" to "-11421879",
+            "Sage" to "-8722497",
+            "Peacock" to "-12134693",
+            "Blueberry" to "-11238163",
+            "Lavender" to "-5980676",
+            "Grape" to "-2380289",
+            "Flamingo" to "-30596",
+            "Graphite" to "-1973791"
+        )
+        var selectedColors = BooleanArray(googleCalendarColorMap.size)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestCalendarPermission()
+      calendarViewModel = ViewModelProviders.of(this)
+          .get(CalendarViewModel::class.java)
     }
-
+    
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        calendarViewModel =
-            ViewModelProviders.of(this).get(CalendarViewModel::class.java)
+
+       val root = inflater.inflate(R.layout.calendar_fragment, container, false)
+
         calendarViewModel.init()
-
-        val root = inflater.inflate(R.layout.calendar_fragment, container, false)
-
         val textView: TextView = root.findViewById(R.id.text_calendar)
         calendarViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        recyclerView = root.findViewById(R.id.calendar_recycler_view)
 
-        initRecyclerView(root)
-        return root
+      val selectCalendarButton: Button = root.findViewById(R.id.select_calendar_button)
+        selectCalendarButton.setOnClickListener {
+            showDialog()
+        }
+
+        calendarViewModel.getCalendars().observe(viewLifecycleOwner , Observer {
+
+            //On Change here. Whenever the data changes the recycler view will be updated
+            updateRecyclerView()
+            recyclerView.adapter!!.notifyDataSetChanged()
+
+        })
+      return root
+    }
+
+    private fun showDialog() {
+
+        val dialog = CalendarFragment()
+        dialog.setTargetFragment(this, targetRequestCode)
+        dialog.show(fragmentManager!!, "select calendars dialog")
+
     }
 
     private fun requestCalendarPermission() {
@@ -70,32 +115,69 @@ class CalendarFragment : Fragment() {
             )
     }
 
-    private fun initRecyclerView(root: View) {
+    private fun updateRecyclerView() {
 
-        val recyclerView: RecyclerView = root.findViewById(R.id.calendar_recycler_view)
+        val events: ArrayList<CalendarEvent> = arrayListOf()
+        for (cal in calendarViewModel.getCalendars().value!!)
+            events.addAll(cal.events)
+        events.sortWith(compareBy { it.startTime })
 
         with(recyclerView) {
             layoutManager = when {
                 columnCount <= 1 -> LinearLayoutManager(context)
                 else -> GridLayoutManager(context, columnCount)
             }
-
-            val calendars: List<Calendar> =
-                calendarViewModel.getUserCalendars().value!!.values.toList()
-            val events: ArrayList<CalendarEvent> = arrayListOf()
-
-            for (cal in calendars) {
-                events.addAll(cal.events)
-            }
-            events.sortWith(compareBy { it.startTime })
-            adapter = CalendarAdapter(
-                events,
-                onCalendarEventClickListener
-            )
+            adapter = CalendarAdapter(events, onCalendarEventClickListener)
         }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let { it ->
+
+            val selectedBool = loadDialog()
+            val colorArray = googleCalendarColorMap.keys.toTypedArray()
+
+            val builder = AlertDialog.Builder(it)
+                .setTitle("Select Your Calendars")
+                .setMultiChoiceItems(colorArray, selectedBool
+                ) { _, which, isChecked ->
+                    selectedBool!![which] = isChecked
+                }.setPositiveButton(R.string.ok) { _, _ ->
+
+                    saveDialog(selectedBool!!)
+                    targetFragment!!.onActivityResult(targetRequestCode, 1, null)
+                }
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
     }
 
     interface OnCalendarEventClickListener {
         fun onCalendarEventClick(item: CalendarEvent?)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        calendarViewModel.selectCalendars(selectedColors)
+
+    }
+
+    private fun saveDialog(checkedArr: BooleanArray) {
+        selectedColors = checkedArr
+        val sharedPreferences = activity?.getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPreferences?.edit()
+        for (i in checkedArr.indices) {
+            editor?.putBoolean(i.toString(), checkedArr[i])
+        }
+        editor?.apply()
+    }
+
+    private fun loadDialog(): BooleanArray? {
+        val sharedPreferences = activity?.getPreferences(Context.MODE_PRIVATE)
+        val checkedArr = BooleanArray(googleCalendarColorMap.size)
+        for (i in checkedArr.indices) {
+            checkedArr[i] = sharedPreferences!!.getBoolean(i.toString(), false)
+        }
+        selectedColors = checkedArr
+        return checkedArr
     }
 }
