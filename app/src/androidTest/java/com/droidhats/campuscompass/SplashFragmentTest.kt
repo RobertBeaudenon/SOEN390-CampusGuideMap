bbab58bc96c8aa2a7fa19330e857bf8be67b4d29
Amanda Kolopanis <amandakolopa@gmail.com>
2020-02-25 22:41:04 -0500

    Updated UI tests where possible navigations are under one test instead of four separate tests.

--- app/src/androidTest/java/com/droidhats/campuscompass/SplashFragmentTest.kt
+++ app/src/androidTest/java/com/droidhats/campuscompass/SplashFragmentTest.kt
@@ -1,7 +1,6 @@
 package com.droidhats.campuscompass
 
 import androidx.navigation.testing.TestNavHostController
-import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.pressBack
@@ -10,27 +9,45 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import junit.framework.TestCase.assertEquals
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class SplashFragmentTest {
 
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity>
+            = ActivityTestRule(MainActivity::class.java)
+
     val navController = TestNavHostController(
         ApplicationProvider.getApplicationContext())
 
-    //Tests if the current view is splash_fragment
+    //Tests possible navigation paths from splash_fragment
     @Test
-    fun test_isFragmentInView() {
-        ActivityScenario.launch(MainActivity::class.java)
+    fun test_isNavigationCorrect() {
         navController.setGraph(R.navigation.navigation)
+
+        //tests if splash_fragment is in view
         assertEquals(navController.currentDestination?.id, R.id.splash_fragment)
+
+        //tests if the side menu is visible
+        onView(withId(R.id.nav_menu))
+            .check(doesNotExist())
+
+        //tests if the back button will navigate away from splash_fragment
+        pressBack()
+        assertEquals(navController.currentDestination?.id, R.id.splash_fragment)
+
+        //tests if there is navigation to the map_fragment
+        navController.navigate(R.id.action_splashFragment_to_mapsActivity)
+        assertEquals(navController.currentDestination?.id, R.id.map_fragment)
     }
 
     //Tests if all images appear on view
     @Test
     fun test_isFragmentImagesInView() {
-        ActivityScenario.launch(MainActivity::class.java)
         //sees if the images display
         onView(withId(R.id.splash_screen))
             .check(matches(isDisplayed()))
@@ -40,31 +57,5 @@ class SplashFragmentTest {
             .check(matches(isDisplayed()))
         onView(withId(R.id.cc_university))
             .check(matches(isDisplayed()))
-    }
-
-    //Tests the navigation from splash_fragment to map_fragment (as opposed to ex. calendar_fragment)
-    @Test
-    fun test_isRedirectedToMap() {
-        ActivityScenario.launch(MainActivity::class.java)
-        navController.setGraph(R.navigation.navigation)
-        navController.navigate(R.id.action_splashFragment_to_mapsActivity)
-        assertEquals(navController.currentDestination?.id, R.id.map_fragment)
-    }
-
-    //Tests if splash_fragment stays in view when back button is pressed
-    @Test
-    fun test_disabledBackButton() {
-        ActivityScenario.launch(MainActivity::class.java)
-        navController.setGraph(R.navigation.navigation)
-        pressBack()
-        assertEquals(navController.currentDestination?.id, R.id.splash_fragment)
-    }
-
-    //Tests if the side menu is available during the splash screen
-    @Test
-    fun test_isSideMenuDisabled() {
-        ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.nav_menu))
-            .check(doesNotExist())
     }
 }
