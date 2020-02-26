93d88b302f8124ed471c25031cb2fcdc9d1e752b
Neeham <Neehamk@gmail.com>
2020-02-25 23:20:25 -0500

    Attempting to fix the toggle switch tests
    In the process of fixing the toggle switch tests.

--- app/src/androidTest/java/com/droidhats/campuscompass/views/MapFragmentTest.kt
+++ app/src/androidTest/java/com/droidhats/campuscompass/views/MapFragmentTest.kt
@@ -1,31 +1,33 @@
 package com.droidhats.campuscompass.views
 
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.filters.LargeTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import com.droidhats.campuscompass.MainActivity
 import com.droidhats.campuscompass.R
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
+@LargeTest
 class MapFragmentTest {
 
-
+    @get: Rule
+    val activityRule = ActivityScenarioRule(MapFragment::class.java)
 
     //Checking if we are at the right layout (map_fragment)
     @Test
     fun test_isActivityInView() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.coordinate_layout)).check(matches(isDisplayed()))
     }
 
     @Test
     fun test_visibilityTextsSwitchToggle() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+    //  val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
         onView(withId(R.id.text_SGW)).check(matches(isDisplayed()));
         onView(withId(R.id.toggle_Campus)).check(matches(isDisplayed()));
