c8561369c21434c216419ae5bad7ec4de6d3eee4
Neeham <Neehamk@gmail.com>
2020-02-28 13:02:49 -0500

    Updated Test file to work
    The map fragment test basically ensures that it is in the correct view, has the search bar on the view, has the toggle button, when pressed the toggle button it is indeed checked and has the text SGW, when pressed again it is indeed unchecked and has the text LOY. Additionally, the toggle button is changed. It was initially a picture that had the text embedded to it. Now it's typed via xml.

--- app/src/androidTest/java/com/droidhats/campuscompass/views/MapFragmentTest.kt
+++ app/src/androidTest/java/com/droidhats/campuscompass/views/MapFragmentTest.kt
@@ -4,32 +4,29 @@ import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
 import com.droidhats.campuscompass.MainActivity
 import com.droidhats.campuscompass.R
-import junit.framework.TestCase
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import junit.framework.TestCase
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MapFragmentTest {
 
-
     @get:Rule
-    var activityRule: ActivityTestRule<MainActivity>
-            = ActivityTestRule(MainActivity::class.java)
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
 
     private lateinit var navController: TestNavHostController
 
     @Before
     fun setUp() {
-        navController = TestNavHostController(
-            ApplicationProvider.getApplicationContext())
+        navController = TestNavHostController(ApplicationProvider.getApplicationContext())
         //needs to be here to see which fragment is being opened
         navController.setGraph(R.navigation.navigation)
     }
@@ -37,21 +34,55 @@ class MapFragmentTest {
     //Checking if we are at the right layout (map_fragment)
     @Test
     fun test_isActivityInView() {
-        navController.setCurrentDestination(R.id.map_fragment)
-        TestCase.assertEquals(navController.currentDestination?.id, R.id.map_fragment)
-        //onView(withId(R.id.coordinate_layout)).check(matches(isDisplayed())) // no clue what was being checked here
+
+        //Ensuring the app starts with splash_fragment
+        if (navController.currentDestination?.id == R.id.splash_fragment) {
+
+            //navigating to map_fragment - You can put either action id or destination id. I chose the action id so I can check that it indeed takes you the specified destination id.
+            navController.navigate(R.id.action_splashFragment_to_mapsActivity);
+
+            //Checking if action id indeed took you to the correct destination id
+            TestCase.assertEquals(navController.currentDestination?.id!!, R.id.map_fragment)
+
+            //Waiting 5 seconds for splash screen to load
+            Thread.sleep(5000);
+
+            //Checking if that action id did take you to map_fragment
+            onView(withId(R.id.coordinate_layout)).check(matches(isDisplayed()))
+        }
     }
 
+    //Checking if the search bar exists
     @Test
-    fun test_visibilityTextsSwitchToggle() {
+    fun test_SearchBar() {
 
-        // refers to items not in project, needs fix
-        onView(withId(R.id.text_SGW)).check(matches(isDisplayed()))
-        onView(withId(R.id.toggle_Campus)).check(matches(isDisplayed()))
-        onView(withId(R.id.text_Loyola)).check(matches(isDisplayed()))
-        onView(withId(R.id.text_SGW)).check(matches(withText("SGW")))
-        onView(withId(R.id.toggle_Campus)).perform(click()).check(matches(isChecked()))
-        onView(withId(R.id.toggle_Campus)).perform(click()).check(matches(isNotChecked()))
-        onView(withId(R.id.text_Loyola)).check(matches(withText("Loyola")))
+        //Moving to map_fragment as well as testing if its indeed there using the previous test we have created.
+        test_isActivityInView()
+
+        //Checking if searchBar exists on map_fragment
+        onView(withId(R.id.searchBar)).check(matches(isDisplayed()))
+    }
+
+
+    @Test
+    fun test_SwitchToggle() {
+
+        //Moving to map_fragment as well as testing if its indeed there using the previous test we have created.
+        test_isActivityInView()
+
+        //Checking if toggle button is displayed
+        onView(withId(R.id.toggleButton)).check(matches(isDisplayed()))
+
+        //Checking when the toggle is clicked, it's indeed checked
+        onView(withId(R.id.toggleButton)).perform(click()).check(matches(isChecked()))
+
+        //Ensuring the text of the toggle just clicked is indeed SWG
+        onView(withId(R.id.toggleButton)).check(matches(withText("SGW")))
+
+        //Checking when the toggle is clicked again, it's indeed not checked
+        onView(withId(R.id.toggleButton)).perform(click()).check(matches(isNotChecked()))
+
+        //Ensuring the text of the toggle just clicked is indeed LOY
+        onView(withId(R.id.toggleButton)).check(matches(withText("LOY")))
     }
 }
