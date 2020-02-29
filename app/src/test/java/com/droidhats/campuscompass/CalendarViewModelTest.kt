9b1140f2e6fa3feae4ad046590079e797618ed06
robertbeaudenon <robertbeaudenon@gmail.com>
2020-02-29 18:36:24 -0500

    Initializing calendar view model

--- app/src/test/java/com/droidhats/campuscompass/CalendarViewModelTest.kt
+++ app/src/test/java/com/droidhats/campuscompass/CalendarViewModelTest.kt
@@ -1,7 +1,14 @@
 package com.droidhats.campuscompass
 
+
+import android.app.Application
+import android.content.Context
+import android.os.Build
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.ViewModelProviders
 import com.droidhats.campuscompass.viewmodels.CalendarViewModel
+import com.droidhats.campuscompass.views.CalendarFragment
+import com.droidhats.campuscompass.MainActivity
 
 import org.junit.Test
 import org.junit.Before
@@ -9,28 +16,36 @@ import org.junit.Rule
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.junit.ClassRule
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
 import java.util.*
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 
-@RunWith(JUnit4::class)
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
 class CalendarViewModelTest {
 
-    // Necessary for instantiating view model
-    @Rule
-    @JvmField
-    val instantExecutorRule = InstantTaskExecutorRule()
+    var calendarViewModel:  CalendarViewModel
 
-    // Necessary for instantiating view model
-    companion object {
-        @ClassRule
-        @JvmField
-        val schedulers = RxImmediateSchedulerRule()
-    }
 
-    private lateinit var calendarViewModel: CalendarViewModel
+   //initializing the calendarviewmodel
+    init {
+       val context: Application = RuntimeEnvironment.application
+        calendarViewModel = CalendarViewModel(context)
 
-    @Before
-    fun setUp() {
-       // calendarViewModel = CalendarViewModel()
+   }
+
+
+    // Checks the status of the initializations to make sure they return true
+    @Test
+    fun testEvents() {
+
+
+
+
+
     }
 
 
