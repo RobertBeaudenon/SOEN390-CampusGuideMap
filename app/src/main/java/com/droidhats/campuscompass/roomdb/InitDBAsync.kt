5171de63db70a72a6001cc81a31f4a468e08e812
robertbeaudenon <robertbeaudenon@gmail.com>
2020-03-08 12:00:44 -0400

    Added a helper class in order to initialize the tables

--- app/src/main/java/com/droidhats/campuscompass/roomdb/InitDBAsync.kt
+++ app/src/main/java/com/droidhats/campuscompass/roomdb/InitDBAsync.kt
@@ -1,4 +1,23 @@
 package com.droidhats.campuscompass.roomdb
 
-class InitDBAsync {
+import android.os.AsyncTask
+
+public class InitDBAsync(db:AppDB): AsyncTask<Void, Void, Void>() {
+
+    //I will use that class to fetch the times from the csv file that i will insert in DB
+
+    private val  shuttlebusDao: ShuttleBus_DAO
+    init{
+        shuttlebusDao =  db.shuttleBusDAO()
+    }
+    override fun doInBackground(vararg params: Void): Void? {
+        //shuttleBus_Dao.deleteAll()
+
+        var loyola = ShuttleBus_Loyola_Entity("F", "14:30")
+        shuttlebusDao.saveLoyolaShuttleTime(loyola)
+
+        var sgw = ShuttleBus_SGW_Entity("MTWT", "16:30")
+        shuttlebusDao.saveSGWShuttleTime(sgw)
+        return null
+    }
 }
