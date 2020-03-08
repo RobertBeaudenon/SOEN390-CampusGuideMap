d60b1603020e304d48e66dd70d94b9149a420179
robertbeaudenon <robertbeaudenon@gmail.com>
2020-03-08 12:01:32 -0400

    Prepared the navigation repo that will access the data stored in DB

--- app/src/main/java/com/droidhats/campuscompass/repositories/NavigationRepository.kt
+++ app/src/main/java/com/droidhats/campuscompass/repositories/NavigationRepository.kt
@@ -1,4 +1,31 @@
 package com.droidhats.campuscompass.repositories
 
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.droidhats.campuscompass.roomdb.AppDB
+import com.droidhats.campuscompass.roomdb.ShuttleBus_DAO
+import com.droidhats.campuscompass.roomdb.ShuttleBus_Loyola_Entity
+import com.droidhats.campuscompass.roomdb.ShuttleBus_SGW_Entity
+
 class NavigationRepository {
+
+    private  var shuttleBusDAO: ShuttleBus_DAO
+    private  var loyolaShuttleTimes: LiveData<List<ShuttleBus_Loyola_Entity>>
+    private  var sgwShuttleTimes: LiveData<List<ShuttleBus_SGW_Entity>>
+
+    constructor(application: Application){
+        val db = AppDB.getInstance(application)
+        shuttleBusDAO = db!!.shuttleBusDAO()
+        loyolaShuttleTimes = shuttleBusDAO.getLoyolaShuttleTime()
+        sgwShuttleTimes = shuttleBusDAO.getSGWShuttleTime()
+    }
+
+    //to access times from View model
+    fun getLoyolaShuttleTime(): LiveData<List<ShuttleBus_Loyola_Entity>> {
+        return loyolaShuttleTimes
+    }
+
+    fun getSGWShuttleTime(): LiveData<List<ShuttleBus_SGW_Entity>> {
+        return sgwShuttleTimes
+    }
 }
