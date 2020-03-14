9c52b73fb6d602a3cc8710d4a57538bd289d130b
Robert <robertbeaudenon@gmail.com>
2020-03-14 13:06:28 -0400

    Prepared View model class to retrieve data from Repository

--- app/src/main/java/com/droidhats/campuscompass/viewmodels/NavigationViewModel.kt
+++ app/src/main/java/com/droidhats/campuscompass/viewmodels/NavigationViewModel.kt
@@ -1,4 +1,29 @@
 package com.droidhats.campuscompass.viewmodels
 
-class NavigationViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.droidhats.campuscompass.repositories.NavigationRepository
+import com.droidhats.campuscompass.roomdb.ShuttleBus_Loyola_Entity
+import com.droidhats.campuscompass.roomdb.ShuttleBus_SGW_Entity
+
+class NavigationViewModel: AndroidViewModel{
+
+    private  var navigationRepository: NavigationRepository
+    public  var loyolaShuttleTimes: LiveData<List<ShuttleBus_Loyola_Entity>>
+    private  var sgwShuttleTimes: LiveData<List<ShuttleBus_SGW_Entity>>
+
+    constructor(application: Application) : super(application){
+        navigationRepository = NavigationRepository(application)
+        loyolaShuttleTimes  =  navigationRepository.getLoyolaShuttleTime()
+        sgwShuttleTimes = navigationRepository.getSGWShuttleTime()
+    }
+
+    fun getLoyolaShuttleTime(): LiveData<List<ShuttleBus_Loyola_Entity>> {
+        return loyolaShuttleTimes
+    }
+
+    fun getSGWShuttleTime(): LiveData<List<ShuttleBus_SGW_Entity>> {
+        return sgwShuttleTimes
+    }
 }
