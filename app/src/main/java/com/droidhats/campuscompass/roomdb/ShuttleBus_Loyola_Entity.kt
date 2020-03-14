39571924f5a6dbc42902639f37008ec14d8f4086
Robert <robertbeaudenon@gmail.com>
2020-03-14 13:05:39 -0400

    Modified entities to match schema predefined in file asset

--- app/src/main/java/com/droidhats/campuscompass/roomdb/ShuttleBus_Loyola_Entity.kt
+++ app/src/main/java/com/droidhats/campuscompass/roomdb/ShuttleBus_Loyola_Entity.kt
@@ -3,15 +3,25 @@ package com.droidhats.campuscompass.roomdb
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
 
-@Entity(primaryKeys = ["SHUTTLE_DAY","SHUTTLE_TIME"])
-class ShuttleBus_Loyola_Entity (
+@Entity()
+data class ShuttleBus_Loyola_Entity (
+
+    @PrimaryKey
+    @NotNull
+    @ColumnInfo(name = "ID")
+    var ID: Int,
+
+    @NotNull
     @ColumnInfo(name ="SHUTTLE_DAY")
     var shuttle_day: String= "",
 
 
+    @NotNull
     @ColumnInfo(name ="SHUTTLE_TIME")
     var shuttle_time: String= ""
 
-){
-}
+){}
+
+
--- app/src/main/java/com/droidhats/campuscompass/roomdb/ShuttleBus_SGW_Entity.kt
+++ app/src/main/java/com/droidhats/campuscompass/roomdb/ShuttleBus_SGW_Entity.kt
@@ -3,16 +3,25 @@ package com.droidhats.campuscompass.roomdb
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
 
-@Entity(primaryKeys = ["SHUTTLE_DAY","SHUTTLE_TIME"])
-class ShuttleBus_SGW_Entity(
+@Entity()
+data class ShuttleBus_SGW_Entity(
+
+    @PrimaryKey
+    @NotNull
+    @ColumnInfo(name = "ID")
+    var ID: Int,
+
 
     @ColumnInfo(name ="SHUTTLE_DAY")
+    @NotNull
     var shuttle_day: String= "",
 
 
     @ColumnInfo(name ="SHUTTLE_TIME")
-var shuttle_time: String= ""
+    @NotNull
+    var shuttle_time: String= ""
 
 ) {
 
