54ce9ad5ed5e003ef6f1f139a167debf8c4acac4
Robert <robertbeaudenon@gmail.com>
2020-03-17 09:26:46 -0400

    Tested the ShuttleBus Database

--- app/src/test/java/com/droidhats/campuscompass/roomdb/ShuttleBusDBTest.kt
+++ app/src/test/java/com/droidhats/campuscompass/roomdb/ShuttleBusDBTest.kt
@@ -1,4 +1,25 @@
 package com.droidhats.campuscompass.roomdb
 
+import android.os.Build
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import org.junit.Assert
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
 class ShuttleBusDBTest {
+    private val shuttlebusDB: ShuttleBusDB= ShuttleBusDB.getInstance(RuntimeEnvironment.application)
+
+    @Test
+    fun testSingleton() {
+        val testDB: ShuttleBusDB= ShuttleBusDB.getInstance(RuntimeEnvironment.application)
+        
+        Assert.assertEquals(
+            testDB.toString(),
+            shuttlebusDB.toString()
+        )
+    }
 }
