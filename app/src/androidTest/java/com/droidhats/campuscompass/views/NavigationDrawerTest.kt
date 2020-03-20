70b7b9a40598deb49241c9c5d39d042599c4e664
Neeham <Neehamk@gmail.com>
2020-03-20 14:41:23 -0400

    Optimized Imports
    - Changed any imports that were using .*

--- app/src/androidTest/java/com/droidhats/campuscompass/views/NavigationDrawerTest.kt
+++ app/src/androidTest/java/com/droidhats/campuscompass/views/NavigationDrawerTest.kt
@@ -2,19 +2,19 @@ package com.droidhats.campuscompass.views
 
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerMatchers
-import androidx.test.uiautomator.UiDevice
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import androidx.test.uiautomator.UiDevice
 import com.droidhats.campuscompass.MainActivity
 import com.droidhats.campuscompass.R
-import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
