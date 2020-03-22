6a5a8c9e883a6a0b5847eb4e5a9cb61cba0f67a3
Nicholas Nagy <ygannicholas@gmail.com>
2020-03-22 03:19:02 -0400

    Displaying map on floor fragment
    In this commit, I added the necessary libraries and functionality in
    order to display the map on the floor fragment when you click on a
    button at the top of the screen.
    
    This is a temporary set up, just to get eveything working.

--- app/src/main/java/com/droidhats/campuscompass/views/FloorFragment.kt
+++ app/src/main/java/com/droidhats/campuscompass/views/FloorFragment.kt
@@ -1,15 +1,23 @@
 package com.droidhats.campuscompass.views
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.graphics.drawable.VectorDrawable
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.caverock.androidsvg.SVG
 import com.droidhats.campuscompass.R
 import com.droidhats.campuscompass.viewmodels.FloorViewModel
+import java.io.InputStream
+
 
 class FloorFragment : Fragment() {
 
@@ -19,15 +27,27 @@ class FloorFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.floor_fragment, container, false)
+        var root = inflater.inflate(R.layout.floor_fragment, container, false)
+
+        var randomPathBtn: Button = root.findViewById(R.id.randomPathBtn)
+        val inputStream: InputStream = requireContext().assets.open("hall8.svg")
+        randomPathBtn.setOnClickListener{it ->
+            val svg: SVG = SVG.getFromInputStream(inputStream)
+            var imageView: ImageView = root.findViewById(R.id.floormap)
+            svg.setDocumentWidth(imageView.width.toFloat())
+            val bitmap = Bitmap.createBitmap(imageView.width, imageView.height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            canvas.drawARGB(0, 255, 255, 255)
+            svg.renderToCanvas(canvas)
+            imageView.setImageDrawable(BitmapDrawable(getResources(), bitmap))
+        }
+
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(FloorViewModel::class.java)
-
-
-        var vd : VectorDrawable
     }
 
 }
