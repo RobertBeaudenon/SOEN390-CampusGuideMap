c57caab304fef8bbb9d84f6b21a226c23a97481c
Neeham <Neehamk@gmail.com>
2020-03-29 11:26:25 -0400

    Fixed instructions being shown
    - Instructions have been fixed to show the correct order of steps to follow.

--- app/src/main/java/com/droidhats/campuscompass/repositories/NavigationRepository.kt
+++ app/src/main/java/com/droidhats/campuscompass/repositories/NavigationRepository.kt
@@ -42,7 +42,6 @@ class NavigationRepository(private val application: Application) {
     var routeTimes = MutableLiveData<MutableMap<String, String>>()
 
 
-
     companion object {
         // Singleton instantiation
         private var instance: NavigationRepository? = null
@@ -76,7 +75,7 @@ class NavigationRepository(private val application: Application) {
         return sgwShuttleTimes
     }
 
-    suspend fun fetchPlace(location: Location) : Unit = suspendCoroutine  { cont->
+    suspend fun fetchPlace(location: Location): Unit = suspendCoroutine { cont ->
         if (location is GooglePlace) {
             val placeFields: List<Place.Field> = Place.Field.values().toList()
             val placesClient = Places.createClient(application.applicationContext)
@@ -120,7 +119,7 @@ class NavigationRepository(private val application: Application) {
                         times[method.string] = "N/A"
                     }
                     //Set only after all the times have been retrieved (to display them all at the same time)
-                    if(times.size == NavigationRoute.TransportationMethods.values().size )
+                    if (times.size == NavigationRoute.TransportationMethods.values().size)
                         routeTimes.value = times
                 },
                 Response.ErrorListener {
@@ -134,8 +133,8 @@ class NavigationRepository(private val application: Application) {
     }
 
     fun generateDirections(origin: Location, destination: Location, mode: String) {
-         val instructions = arrayListOf<String>()
-        if(origin.coordinate == LatLng(0.0, 0.0) || destination.coordinate == LatLng(0.0, 0.0))
+        val instructions = arrayListOf<String>()
+        if (origin.coordinate == LatLng(0.0, 0.0) || destination.coordinate == LatLng(0.0, 0.0))
             return
         val directionsRequest = object : StringRequest(
             Method.GET,
@@ -162,21 +161,51 @@ class NavigationRepository(private val application: Application) {
 
                         try {
                             if (mode == "transit" || mode == "walking") {
-                                instructions.add(stepsArray.getJSONObject(i).getString("html_instructions") + "<br><br>Distance: " + stepsArray.getJSONObject(i).getJSONObject("distance").getString("text") + "<br><br>Duration: " + stepsArray.getJSONObject(i).getJSONObject("duration").getString("text"))
+                                if (stepsArray.getJSONObject(i).has("transit_details")) {
+                                    instructions.add(
+                                        stepsArray.getJSONObject(i)
+                                            .getString("html_instructions") + "<br><Distance: " + stepsArray.getJSONObject(
+                                            i
+                                        ).getJSONObject("distance")
+                                            .getString("text") + "<br>Duration: " + stepsArray.getJSONObject(
+                                            i
+                                        ).getJSONObject("duration")
+                                            .getString("text") + "<br>Arrival Stop: " + stepsArray.getJSONObject(
+                                            i
+                                        ).getJSONObject("transit_details")
+                                            .getJSONObject("arrival_stop")
+                                            .getString("name") + "<br>Total Number of Stop: " + stepsArray.getJSONObject(
+                                            i
+                                        ).getJSONObject("transit_details").getString("num_stops")
+                                    )
+                                } else {
+                                    instructions.add(
+                                        stepsArray.getJSONObject(i)
+                                            .getString("html_instructions") + "<br><br>Distance: " + stepsArray.getJSONObject(
+                                            i
+                                        ).getJSONObject("distance")
+                                            .getString("text") + "<br><br>Duration: " + stepsArray.getJSONObject(
+                                            i
+                                        ).getJSONObject("duration").getString("text")
+                                    )
+                                }
                                 if (stepsArray.getJSONObject(i).has("steps")) {
-                                    for (j in 0 until stepsArray.getJSONObject(i).getJSONArray("steps").length()) {
-                                        instructions.add(stepsArray.getJSONObject(i).getJSONArray("steps").getJSONObject(j).getString("html_instructions"))
+                                    for (j in 0 until stepsArray.getJSONObject(i)
+                                        .getJSONArray("steps").length()) {
+                                        instructions.add(
+                                            stepsArray.getJSONObject(i).getJSONArray("steps")
+                                                .getJSONObject(j).getString("html_instructions")
+                                        )
                                     }
                                 }
-                                if (stepsArray.getJSONObject(i).has("transit_details")) {
-                                    //instructions.add("Departure Stop: " + stepsArray.getJSONObject(i).getJSONObject("transit_details").getJSONObject("departure_stop").getString("name") + "<br><br>Arrival Stop: " + stepsArray.getJSONObject(i).getJSONObject("transit_details").getJSONObject("arrival_stop").getString("name") + "<br><br>Total Number of Stop: " + stepsArray.getJSONObject(i).getJSONObject("transit_details").getString("num_stops"))
-                                }
                             } else {
-                                instructions.add(stepsArray.getJSONObject(i).getString("html_instructions"))
+                                instructions.add(
+                                    stepsArray.getJSONObject(i).getString("html_instructions")
+                                )
                             }
                             path.add(PolyUtil.decode(points))
-                        }catch (e : org.json.JSONException){
-                            Log.e("JSONException" , e.message.toString())
+                        } catch (e: org.json.JSONException) {
+                            Log.e("JSONException", e.message.toString())
                         }
                     }
                     val navigation = NavigationRoute(origin, destination, mode, path, instructions)
@@ -193,7 +222,11 @@ class NavigationRepository(private val application: Application) {
     }
 
 
-    private fun constructRequestURL(origin: Location, destination: Location, transportationMethod : String) : String {
+    private fun constructRequestURL(
+        origin: Location,
+        destination: Location,
+        transportationMethod: String
+    ): String {
         return "https://maps.googleapis.com/maps/api/directions/json?" +
                 "origin=" + origin.coordinate.latitude.toString() + "," + origin.coordinate.longitude.toString() +
                 "&destination=" + destination.coordinate.latitude.toString() + "," + destination.coordinate.longitude.toString() +
@@ -201,5 +234,5 @@ class NavigationRepository(private val application: Application) {
                 "&key=" + application.applicationContext.getString(R.string.ApiKey)
     }
 
-    fun getNavigationRoute() : MutableLiveData<NavigationRoute> = navigationRoute
+    fun getNavigationRoute(): MutableLiveData<NavigationRoute> = navigationRoute
 }
