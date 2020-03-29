bfab24a579a8340cd2583132f4efe338274fc7e5
Nicholas Nagy <ygannicholas@gmail.com>
2020-03-29 13:11:58 -0400

    Added more info for elements and added debugging code
    I added code to be able to write to a file the new svg content to be able to debug the application.
    
    I also added more functionality to be able to create points, paths and rectangles on the map.
    
    At this point, I am covering the background with points.

--- app/src/main/java/com/droidhats/mapprocessor/ProcessMap.kt
+++ app/src/main/java/com/droidhats/mapprocessor/ProcessMap.kt
@@ -2,9 +2,11 @@ package com.droidhats.mapprocessor
 import java.io.File
 
 class ProcessMap {
-    private var rectangles: MutableList<Rect> = mutableListOf()
-    private var paths: MutableList<Path> = mutableListOf()
+    //    private var rectangles: MutableList<Rect> = mutableListOf()
+//    private var paths: MutableList<Path> = mutableListOf()
+    private var classes: MutableList<MapElement> = mutableListOf()
     private var firstElement: MapElement? = null
+    private var finalPath: MutableList<Circle> = mutableListOf()
 
     fun readSVG() {
         var element: String = ""
@@ -20,10 +22,10 @@ class ProcessMap {
 
             if (it.contains("/>") && inRect) {
                 if (firstElement) {
-                    this.firstElement = createRect(it)
+                    this.firstElement = createRect(element)
                     firstElement = false
                 } else {
-                    rectangles.add(createRect(it))
+                    classes.add(createRect(element))
                 }
                 inRect = false
                 element = ""
@@ -35,40 +37,42 @@ class ProcessMap {
             if (inPath) element += it
 
             if (it.contains("/>") && inPath) {
+                val path: Path = createPath(element)
                 if (firstElement) {
-                    this.firstElement = createPath(it)
+                    this.firstElement = path
                     firstElement = false
-                } else {
-                    paths.add(createPath(it))
+                } else if (path.isClosed) {
+                    println(path)
+                    classes.add(path)
                 }
                 inPath = false
                 element = ""
             }
         }
-
     }
 
-    fun createRect(it: String): Rect {
-        val id = extractAttr("id", it)!!
-        val x = extractAttr("x", it)!!.toDouble()
-        val y = extractAttr("y", it)!!.toDouble()
-        val height = extractAttr("height", it)!!.toDouble()
-        val width = extractAttr("width", it)!!.toDouble()
-        val style = extractAttr("style", it)!!
+    private fun createRect(it: String): Rect {
+        val id = extractAttr("id", it)
+        val x = extractAttr("x", it).toDouble()
+        val y = extractAttr("y", it).toDouble()
+        val height = extractAttr("height", it).toDouble()
+        val width = extractAttr("width", it).toDouble()
+        val style = extractAttr("style", it)
         return Rect(id, x, y, height, width, style)
     }
 
-    fun createPath(it: String): Path {
-        val id = extractAttr("id", it)!!
-        val d = extractAttr("d", it)!!
-        val style = extractAttr("style", it)!!
-        val isClosed = d.contains("z")
+    private fun createPath(it: String): Path {
+        val id = extractAttr("id", it)
+        val d = extractAttr("d", it)
+        val style = extractAttr("style", it)
+        val isClosed = d[d.length - 1] == 'z'
+        println("d: $d")
         return Path(id, d, style, isClosed)
     }
 
-    fun extractAttr(attribute: String, line: String): String? {
-        val string: String = "$attribute="
-        if (!line.contains(string)) return null
+    private fun extractAttr(attribute: String, line: String): String {
+        val string: String = " $attribute="
+        if (!line.contains(string)) return ""
 
         var inString: Boolean = false
         var startExtractingString: Boolean = false
@@ -95,6 +99,86 @@ class ProcessMap {
             }
         }
         return value
+    }
+
+    fun writeSVG() {
+        val writeFile = File("writeFile.svg")
+        var wrote: Boolean = false
+        File("Hall-8.svg").forEachLine { it ->
+            if (it.contains("</g>") && !wrote) {
+                writeFile.appendText(createText() + "\n")
+                wrote = true
+            }
+            writeFile.appendText(it + "\n")
+        }
+    }
+
+    fun createText(): String {
+        var string: String = ""
+        classes.forEach{ it ->
+            if (it.toString()[1] == 'p') {
+
+                val center: Pair<Double, Double> = it.getCenter()
+                println("center: $center")
+                string += Circle(center.first, center.second, 10.0)
+            } else {
+
+                val center: Pair<Double, Double> = it.getCenter()
+                string += Circle(center.first, center.second, 5.0)
+            }
+        }
+        return string
+    }
+
+    fun coverBackground(): String {
+        var string: String = ""
+        var circles: MutableList<Circle> = mutableListOf()
+
+        val span = firstElement!!.getWidth()
+        val spanHeight = firstElement!!.getHeight()
+        for (i in span.first.toInt() until span.second.toInt() step 15) {
+            for (y in spanHeight.first.toInt() until spanHeight.second.toInt() step 15) {
+                var notInPath = false
+                classes.forEach{rect ->
+                    if(rect.isWithin(i.toDouble(), y.toDouble())) notInPath = true
+                }
+
+                if (notInPath) continue
+                circles.add(Circle.getPoint(i.toDouble(), y.toDouble()))
+            }
+        }
+
+        var i: Int = 0
+        while (i < (circles.size - 1)) {
+            var circlesList: MutableList<Circle> = mutableListOf()
+            var x: Int = 0
+            while (x < circles.size - 1) {
+                println(circles[x])
+                val circle: Circle = circles[x]
+                if (circles[i].isWithin(circle.cx, circle.cy, 35.0)) {
+                    circlesList.add(circle)
+                    circles.removeAt(x)
+                }
+                x++
+            }
+            string += findCenter(circlesList)
+            i++
+        }
+
+        return string
+    }
+
+    fun findCenter(pointList: MutableList<Circle>): Circle {
+        var xSum: Double = 0.0
+        var ySum: Double = 0.0
+        val size = pointList.size
+
+        for (i in 0 until size) {
+            xSum += pointList[i].cx
+            ySum += pointList[i].cy
+        }
+
+        return Circle.getPoint(xSum/size, ySum/size)
     }
 
 }
--- app/src/main/java/com/droidhats/mapprocessor/SVGClasses.kt
+++ app/src/main/java/com/droidhats/mapprocessor/SVGClasses.kt
@@ -1,17 +1,146 @@
 package com.droidhats.mapprocessor
 
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 abstract class MapElement(){
     abstract override fun toString(): String
+    abstract fun isWithin(x: Double, y: Double): Boolean
+    abstract fun getWidth(): Pair<Double, Double>
+    abstract fun getHeight(): Pair<Double, Double>
+    abstract fun getCenter(): Pair<Double, Double>
 }
 
 class Rect (val id: String, val x: Double, val y: Double, val height: Double, val width: Double, val style: String) : MapElement() {
     override fun toString(): String {
         return "<rect id=\"$id\" x=\"$x\" y=\"$y\" height=\"$height\" width=\"$width\" style=\"$style\" />"
     }
+
+    override fun isWithin(x: Double, y: Double): Boolean {
+        return (x > this.x - 5 && x < this.x + width + 5 && y > this.y - 5 && y < this.y + this.height + 5)
+    }
+
+    override fun getWidth(): Pair<Double, Double> {
+        return Pair<Double,Double> (x, x + width)
+    }
+
+    override fun getHeight(): Pair<Double, Double> {
+        return Pair<Double,Double> (y, y + height)
+    }
+
+    override fun getCenter(): Pair<Double, Double> {
+        return Pair<Double, Double> (x + width/2, y + height/2)
+    }
+
 }
 
 class Path(val id: String, val d: String, val style: String, val isClosed: Boolean) : MapElement() {
+
+    var xMin: Double
+    var xMax: Double
+    var yMin: Double
+    var yMax: Double
+    var vertices: MutableList<Pair<Double, Double>> = mutableListOf()
+
+    init {
+        if (d[0] == 'm' && isClosed) {
+            var anArray = d.substring(2, d.length-2).split(" ")
+            println(d)
+            var prevVertex = Pair<Double, Double>(0.0, 0.0)
+            anArray.forEach{ it ->
+                println(it)
+                var element = it.split(",")
+                println(element)
+                var vert = Pair<Double, Double>(element[0].toDouble() + prevVertex.first, element[1].toDouble() + prevVertex.second)
+                vertices.add(Pair<Double, Double>(element[0].toDouble() + prevVertex.first, element[1].toDouble() + prevVertex.second))
+                prevVertex = vert
+            }
+
+            var initialVertex = vertices[0]
+            xMax = initialVertex.first
+            xMin = xMax
+            yMax = initialVertex.second
+            yMin = yMax
+
+            for (vertex in vertices) {
+                if (vertex.first > xMax) xMax = vertex.first
+                if (vertex.first < xMin) xMin = vertex.first
+                if (vertex.second > yMax) yMax = vertex.second
+                if (vertex.second < yMin) yMin = vertex.second
+            }
+        } else {
+            xMin = -1.0
+            xMax = -1.0
+            yMin = -1.0
+            yMax = -1.0
+        }
+    }
+
     override fun toString(): String {
         return "<path id=\"$id\" d=\"$d\" style=\"$style\"/>"
     }
+
+    override fun isWithin(x: Double, y: Double): Boolean {
+
+        if (xMin < 0 || xMax < 0 || yMin < 0 || yMax < 0) {
+            return false
+        }
+
+        if (x < xMin + 5 || x > xMax - 5 || y < yMin + 5 || y > yMax - 5) {
+            return true
+        }
+        return false
+    }
+
+    override fun getWidth(): Pair<Double, Double> {
+        return Pair<Double,Double> (0.0,0.0)
+    }
+
+    override fun getHeight(): Pair<Double, Double> {
+        return Pair<Double,Double> (0.0,0.0)
+    }
+
+    override fun getCenter(): Pair<Double, Double> {
+        var xSum: Double = 0.0
+        var ySum: Double = 0.0
+        for (vertex in vertices) {
+            xSum += vertex.first
+            ySum += vertex.second
+        }
+        return Pair(xSum/vertices.size, ySum/vertices.size)
+    }
+}
+
+class Circle(val cx: Double, val cy: Double, val r: Double) : MapElement() {
+    override fun toString(): String {
+        return "<circle cx=\"$cx\" cy=\"$cy\" r=\"$r\" />"
+    }
+
+    companion object {
+        fun getPoint(x: Double, y: Double): Circle {
+            return Circle(x, y, 5.0)
+        }
+    }
+
+    override fun isWithin(x: Double, y: Double): Boolean {
+        return false
+    }
+
+    fun isWithin(x: Double, y: Double, range: Double): Boolean {
+        val distance: Double = sqrt(abs(cx - x).pow(2.0) + abs(cy - y).pow(2.0))
+        return distance < range
+    }
+    override fun getWidth(): Pair<Double, Double> {
+        return Pair<Double,Double> (0.0,0.0)
+    }
+
+    override fun getHeight(): Pair<Double, Double> {
+        return Pair<Double,Double> (0.0,0.0)
+    }
+
+    override fun getCenter(): Pair<Double, Double> {
+        return Pair<Double, Double>(cx, cy)
+    }
+
 }
