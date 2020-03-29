2092373ab07d341038f8dd31afd7aa8e7b1ed5fe
Zohal <zohalmir9@gmail.com>
2020-03-29 15:00:55 -0400

    Reintroduction of removed import for button

--- app/src/main/java/com/droidhats/campuscompass/views/CalendarFragment.kt
+++ app/src/main/java/com/droidhats/campuscompass/views/CalendarFragment.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.core.view.GravityCompat
