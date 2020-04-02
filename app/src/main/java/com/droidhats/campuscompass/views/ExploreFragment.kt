225d226b375f7dd6e2db7c1cff626e66bde049b8
Robert <robertbeaudenon@gmail.com>
2020-04-02 16:22:13 -0400

    Added navigation from explore fragment back to navigation menu

--- app/src/main/java/com/droidhats/campuscompass/views/ExploreFragment.kt
+++ app/src/main/java/com/droidhats/campuscompass/views/ExploreFragment.kt
@@ -1,4 +1,34 @@
 package com.droidhats.campuscompass.views
 
-class ExploreFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.droidhats.campuscompass.R
+
+class ExploreFragment: Fragment() {
+
+    private lateinit var root : View
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        //launching the explore fragment
+        root = inflater.inflate(R.layout.explore_fragment, container, false)
+
+
+        //adding navigation to menu through the burger icon
+        val sideDrawerButton: ImageButton = root.findViewById(R.id.button_menu_explore)
+        sideDrawerButton.setOnClickListener {
+            requireActivity().findViewById<DrawerLayout>(R.id.drawer_layout).openDrawer(
+                GravityCompat.START)
+        }
+        return root
+    }
 }
