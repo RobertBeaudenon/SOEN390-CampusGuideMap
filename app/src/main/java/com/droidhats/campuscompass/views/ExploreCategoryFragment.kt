110472f85a695ed2674d20921181f225c5820147
Robert <robertbeaudenon@gmail.com>
2020-04-03 09:59:05 -0400

    Added navigation from category back to explore menu fragment

--- app/src/main/java/com/droidhats/campuscompass/views/ExploreCategoryFragment.kt
+++ app/src/main/java/com/droidhats/campuscompass/views/ExploreCategoryFragment.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.droidhats.campuscompass.R
 
 class ExploreCategoryFragment: Fragment() {
@@ -18,6 +23,11 @@ class ExploreCategoryFragment: Fragment() {
         //launching the explore fragment
         root = inflater.inflate(R.layout.explore_category_fragment, container, false)
 
+        //adding navigation to explore menu through the burger icon
+        val sideDrawerButton: ImageButton = root.findViewById(R.id.button_explore)
+        sideDrawerButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
 
         return root
     }
