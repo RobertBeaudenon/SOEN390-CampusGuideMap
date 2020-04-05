d737e3f58308b79ff172852f09aca3ebe4c64f71
Robert <robertbeaudenon@gmail.com>
2020-04-05 11:25:39 -0400

    Displayed corresponding image for explore place

--- app/src/main/java/com/droidhats/campuscompass/adapters/ExplorePlaceAdapter.kt
+++ app/src/main/java/com/droidhats/campuscompass/adapters/ExplorePlaceAdapter.kt
@@ -1,15 +1,18 @@
 package com.droidhats.campuscompass.adapters
 
-import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.droidhats.campuscompass.R
 import com.droidhats.campuscompass.roomdb.ExplorePlaceEntity
 import kotlinx.android.synthetic.main.explore_recycler_item.view.*
+
 
 class ExplorePlaceAdapter(private val items: ArrayList<ExplorePlaceEntity>):
     RecyclerView.Adapter<ExplorePlaceAdapter.ViewHolder>() {
@@ -24,8 +27,12 @@ class ExplorePlaceAdapter(private val items: ArrayList<ExplorePlaceEntity>):
 
         holder.titleView.text = item.name
         holder.rateView.text = item.rating.toString()
-
         holder.locationView.text = if (item.address.isNullOrBlank()) "None" else item.address
+
+        val drawable =
+            ContextCompat.getDrawable(holder.view.context, getPlaceImageResourceID(item.name))
+        holder.imageView.setImageDrawable(drawable)
+
 
        // holder.cardView.setCardBackgroundColor(item.color!!.toInt())
 
@@ -47,8 +54,29 @@ class ExplorePlaceAdapter(private val items: ArrayList<ExplorePlaceEntity>):
         val titleView: TextView = view.place_title_item
         val rateView: TextView = view.place_rating_item
         val locationView: TextView = view.place_location_item
+        val imageView: ImageView = view.place_image
         var cardView: CardView = view.findViewById(R.id.explore_card_view)
       //  var navButton : ImageButton = view.findViewById(R.id.navigateFromEvent)
 
+    }
+
+    /**
+     * Returns the explore place image from drawable resources
+     * @param placeName: Used to map the place name to the place image.
+     */
+    fun getPlaceImageResourceID(placeName: String): Int {
+
+        // The id for the place image resource is of Int type
+        // Return the place image resource id that corresponds to the place name
+        return when (placeName) {
+            "Restaurant Maison Prathet Thai" -> R.drawable.resto_restaurantmaisonprathetthai
+            "Les Saisons de Corée" -> R.drawable.resto_lessaisonsdecoree
+            "Bar-B-Barn" -> R.drawable.resto_bar_b_barn
+            "Kazu" -> R.drawable.resto_kazu
+            "Bacaro Pizzeria" -> R.drawable.resto_bacaropizzeria
+            "Garage Beirut" -> R.drawable.resto_garagebeirut
+            "Da Vinci Ristorante" -> R.drawable.resto_davinciristorante
+            else -> Log.v("ImageError", "couldn't load image")
+        }
     }
 }
