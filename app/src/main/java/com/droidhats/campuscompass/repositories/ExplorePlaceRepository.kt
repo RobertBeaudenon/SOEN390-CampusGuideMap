db11368419b58a561e6a9992d1d2e0b3ad93f785
Robert <robertbeaudenon@gmail.com>
2020-04-05 02:11:00 -0400

    Added access methods to the explore db

--- app/src/main/java/com/droidhats/campuscompass/repositories/ExplorePlaceRepository.kt
+++ app/src/main/java/com/droidhats/campuscompass/repositories/ExplorePlaceRepository.kt
@@ -1,4 +1,43 @@
 package com.droidhats.campuscompass.repositories
 
-class ExplorePlaceRepository {
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.droidhats.campuscompass.roomdb.ExplorePlaceDAO
+import com.droidhats.campuscompass.roomdb.ExplorePlaceDB
+import com.droidhats.campuscompass.roomdb.ExplorePlaceEntity
+
+/**
+ * This class will create a connection with the SQLite DB in order to get the
+ * Places
+ * @param application
+ */
+class ExplorePlaceRepository (private val application: Application)  {
+
+    private var explorePlaceDAO: ExplorePlaceDAO
+    private var allPlaces: LiveData<List<ExplorePlaceEntity>>
+
+    companion object {
+        // Singleton instantiation
+        private var instance: ExplorePlaceRepository? = null
+
+        fun getInstance(application: Application) =
+            instance
+                ?: synchronized(this) {
+                    instance
+                        ?: ExplorePlaceRepository(application).also { instance = it }
+                }
+    }
+
+    init {
+        val db = ExplorePlaceDB.getInstance(application)
+        explorePlaceDAO = db.ExplorePlaceDAO()
+        allPlaces = explorePlaceDAO.getAllPlaces()
+    }
+
+    /**
+     * @return allPlaces
+     */
+    fun getAllPlaces(): LiveData<List<ExplorePlaceEntity>> {
+        return allPlaces
+    }
 }
--- app/src/main/java/com/droidhats/campuscompass/viewmodels/ExplorePlaceViewModel.kt
+++ app/src/main/java/com/droidhats/campuscompass/viewmodels/ExplorePlaceViewModel.kt
@@ -1,4 +1,30 @@
 package com.droidhats.campuscompass.viewmodels
 
-class ExplorePlaceViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.droidhats.campuscompass.repositories.ExplorePlaceRepository
+import com.droidhats.campuscompass.roomdb.ExplorePlaceEntity
+
+/**
+ * This class will provide places to explore to the fragment class
+ * And will interact with the ExplorePlacesRepository to fetch the data
+ * @param application
+ */
+class ExplorePlaceViewModel (application: Application) : AndroidViewModel(application){
+
+    private  var explorePlaceRepository: ExplorePlaceRepository = ExplorePlaceRepository.getInstance(getApplication())
+    private  var allPlaces: LiveData<List<ExplorePlaceEntity>>
+
+
+    init {
+        allPlaces  =  explorePlaceRepository.getAllPlaces()
+    }
+
+    /**
+     * @return allPlaces
+     */
+    fun getAllPlaces(): LiveData<List<ExplorePlaceEntity>> {
+        return allPlaces
+    }
 }
