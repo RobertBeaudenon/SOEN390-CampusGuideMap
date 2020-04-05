7e905f41ee4225e7428cde74bc2230d9d14e596a
Robert <robertbeaudenon@gmail.com>
2020-04-05 04:11:31 -0400

    Adding mapping of places to their images

--- app/src/main/java/com/droidhats/campuscompass/repositories/ExplorePlaceRepository.kt
+++ app/src/main/java/com/droidhats/campuscompass/repositories/ExplorePlaceRepository.kt
@@ -1,7 +1,9 @@
 package com.droidhats.campuscompass.repositories
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.LiveData
+import com.droidhats.campuscompass.R
 import com.droidhats.campuscompass.roomdb.ExplorePlaceDAO
 import com.droidhats.campuscompass.roomdb.ExplorePlaceDB
 import com.droidhats.campuscompass.roomdb.ExplorePlaceEntity
@@ -39,5 +41,25 @@ class ExplorePlaceRepository (private val application: Application)  {
      */
     fun getAllPlaces(): LiveData<List<ExplorePlaceEntity>> {
         return allPlaces
+    }
+
+    /**
+     * Returns the explore place image from drawable resources
+     * @param placeName: Used to map the place name to the place image.
+     */
+    private fun getPlaceImageResourceID(placeName: String): Int {
+
+        // The id for the place image resource is of Int type
+        // Return the place image resource id that corresponds to the place name
+        return when (placeName) {
+            "Restaurant Maison Prathet Thai" -> R.drawable.resto_RestaurantMaisonPrathetThai
+            "Les Saisons de Corée" -> R.drawable.resto_LesSaisonsdeCorée
+            "Bar-B-Barn" -> R.drawable.resto_Bar_B_Barn
+            "Kazu" -> R.drawable.resto_Kazu
+            "Bacaro Pizzeria" -> R.drawable.resto_BacaroPizzeria
+            "Garage Beirut" -> R.drawable.resto_GarageBeirut
+            "Da Vinci Ristorante" -> R.drawable.resto_DaVinciRistorante
+            else -> Log.v("ImageError", "couldn't load image")
+        }
     }
 }
