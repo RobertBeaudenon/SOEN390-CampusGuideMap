7c77ed0c6ca64f6429ef4368d73b17a295982e2b
Amanda-Kolopa <amandakolopa@gmail.com>
2020-04-08 22:20:25 -0400

    #19 Development:
    - Implemented the UI for the Settings option on the side menu
    - Missing UI: images for each option (stairs, escalators, elevators) and putting the Settings option at the bottom of the side menu

---
+++ app/src/main/java/com/droidhats/campuscompass/views/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package com.droidhats.campuscompass.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.droidhats.campuscompass.R
+
+class SettingsFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.settings_fragment, container, false)
+        val sideDrawerButton: ImageButton = root.findViewById(R.id.button_menu)
+        sideDrawerButton.setOnClickListener {
+            requireActivity().findViewById<DrawerLayout>(R.id.drawer_layout).openDrawer(
+                GravityCompat.START
+            )
+        }
+     return root
+    }
+}
